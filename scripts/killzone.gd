17c5205ebcb8d01f1d157942c10ef7ad270e80a3
Rob Griffin <robgriffin247@hotmail.com>
2025-03-14 09:48:36 +0100

    added foundation levels ready for scene switching

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,11 +4,11 @@ class_name Killzone extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	body.is_alive = false
-	Engine.time_scale = 0.4
+	#Engine.time_scale = 0.4
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
+	#Engine.time_scale = 1.0
 	Global.reset_score()
 	get_tree().reload_current_scene()
